249cda36750f292bce4e8226b0d21235faf932a6
MonaElboughdady <48098749+Zaghz@users.noreply.github.com>
2021-10-20 19:40:16 +0200

    comments for code

--- Assignment 1/Matlab Code/elevatormealy.m
+++ Assignment 1/Matlab Code/elevatormealy.m
@@ -1,38 +1,38 @@
 function [state ,y] = elevatormealy(u)
-persistent current_state
-if(isempty(current_state))
+persistent current_state %state variable that persistes with every function call
+if(isempty(current_state)) %initializing the state variable during first function call
     current_state = 1;
 end
-switch current_state
-    case 1
+switch current_state 
+    case 1  %state q1 represents being in the first floor
+        switch u %u is action or event
+            case 1 %floor 1 button is pressed
+                current_state = 1;
+                y = "s"; %elevator remains in place
+            case 2 %floor 2 button is pressed
+                current_state = 2;
+                y = "u1"; %elevator goes up 1 floor
+            otherwise %otherwise floor 3 button is pressed
+                current_state = 3;
+                y = "u2"; %elevator goes up 2 floors
+        end
+    case 2 %state q2 represents being in the second floor
         switch u
             case 1
                 current_state = 1;
-                y = "s";
+                y = "d1"; %elevator goes down 1 floor
             case 2
                 current_state = 2;
-                y = "u1";
-            otherwise
-                current_state = 3;
-                y = "u2";
-        end
-    case 2
-        switch u
-            case 1
-                current_state = 1;
-                y = "d1";
-            case 2
-                current_state = 2;
                 y = "s";
             otherwise
                 current_state = 3;
-                y = "u1";
+                y = "u1"; %elevator goes up 1 floor
         end
-    otherwise
+    otherwise % otherwise state q3 represents being in the third floor
         switch u
             case 1
                 current_state = 1;
-                y = "d2";
+                y = "d2"; %elevator goes down 2 floors
             case 2
                 current_state = 2;
                 y = "d1";
@@ -41,5 +41,5 @@ switch current_state
                 y = "s";
         end
 end
-state = current_state;
+state = current_state; %output current state
 end
--- Assignment 1/Matlab Code/elevatormoore.m
+++ Assignment 1/Matlab Code/elevatormoore.m
@@ -1,19 +1,19 @@
 function [state ,y] = elevatormoore(u)
-persistent current_state
-if(isempty(current_state))
+persistent current_state %state variable that persistes with every function call
+if(isempty(current_state)) %initializing the state variable during first function call
     current_state = 1;
 end
 switch current_state
-    case 1
-        switch u
-            case 1
+    case 1 %state q1 represents being in the first floor
+        switch u %u is action or event
+            case 1 %floor 1 button is pressed
                 current_state = 1;
-            case 2
+            case 2 %floor 2 button is pressed
                 current_state = 4;
-            otherwise
+            otherwise %otherwise floor 3 button is pressed
                 current_state = 6;
         end
-    case 2
+    case 2 %state q2 represents being in the second floor
         switch u
             case 1
                 current_state = 5;
@@ -22,7 +22,7 @@ switch current_state
             otherwise
                 current_state = 8;
         end
-    case 3
+    case 3 % state q3 represents being in the third floor
         switch u
             case 1
                 current_state = 7;
@@ -62,25 +62,25 @@ switch current_state
                 current_state = 2;
         end  
 end
-switch current_state
+switch current_state %choose output depending on current state
     case 1
-        y = "s";
+        y = "s"; %elevator remains in place
     case 2
         y = "s";
     case 3
         y = "s";
     case 4
-        y = "u1";
+        y = "u1"; %elevator goes up 1 floor
     case 5
         y = "u1";
     case 6
-        y = "u2";
+        y = "u2"; %elevator goes up 2 floors
     case 7
-        y = "d2";
+        y = "d2"; %elevator goes down 2 floors
     case 8
         y = "u1";
     case 9
-        y = "d1";
+        y = "d1"; %elevator goes down 1 floor
 end
 state = current_state;
 end
